4f156279506a5d0d3cdfd86a4521c440e8234754
lincoln Duarte <lincolnskyrim_99@hotmail.com>
2025-01-09 14:46:41 -0300

    .

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default function App() {
   return (
     <div className="">
-      <h1>hello</h1>
+      <h1>hellod</h1>
       <MarketingApp />
     </div>
   );
