70cd56b6336d8203593b5b97177ad9d60da15f60
lincoln Duarte <lincolnskyrim_99@hotmail.com>
2025-01-09 14:56:07 -0300

    .

--- container/src/App.js
+++ container/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default function App() {
+  console.log("Domain:", process.env.PRODUCTION_DOMAIN);
   return (
     <div className="">
       <h1>hellold</h1>
