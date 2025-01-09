027e193a8ecba600dcb1b7aaf1b208f1161b2075
lincoln Duarte <lincolnskyrim_99@hotmail.com>
2025-01-09 13:49:29 -0300

    add text

--- container/src/App.js
+++ container/src/App.js
@@ -4,6 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default function App() {
   return (
     <div className="">
+      <h1>hello</h1>
       <MarketingApp />
     </div>
   );
